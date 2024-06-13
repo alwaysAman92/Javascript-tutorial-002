9c1d6e5ab3951335857cb09c3ac10b0f6340d811
alwaysAman92 <134390829+alwaysAman92@users.noreply.github.com>
2024-06-13 13:29:28 +0000

    operations on datatypes

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,5 +1,5 @@
-console.log(typeof undefined); //gives output as undefined
-console.log(typeof null);      //gives output as object
+//console.log(typeof undefined); //gives output as undefined
+//console.log(typeof null);      //gives output as object
 /*
 number = 2 ^ 53
 bigint = for large numbers
@@ -11,3 +11,44 @@ symbol = uniquness
 
 
 # "use strict";   treat all code as newer version of JS */
+
+//***************Operations********************//
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//read about prefix and postfix operation
