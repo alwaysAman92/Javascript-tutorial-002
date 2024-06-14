da5d5abd5f6a882fafcc884bc346842c2f68b5df
alwaysAman92 <134390829+alwaysAman92@users.noreply.github.com>
2024-06-14 17:40:04 +0000

    comparison of datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);*/
+console.log(null>=0);  
+console.log(null===0);
+//both the operator works differently i.e equality and comparison
+//operator comparison operator converts null to zero and check the condition
+console.log(undefined>=0);  
+console.log(undefined==0);
+//but undefined gives false always
+//=== doesn't convert any datatype
