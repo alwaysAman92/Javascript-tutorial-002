8607b6a4100dc1facb6da8a114e4b2e110a05208
alwaysAman92 <134390829+alwaysAman92@users.noreply.github.com>
2024-06-15 15:34:27 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+// primitive (call by value)
+// 7 types-string,number,boolean,null,undefined,symbol,BigInt
+/* undefined means a variable has been declared but has not yet been assigned a value, whereas null is an assignment value, meaning that a variable has been declared and given the value of null .*/
+
+//non primitive or refrence 
+//arrays,objects,functions
+//JavaScript is a dynamically typed language. This means that the type of a variable is not declared when the variable is created, but is instead determined at runtime based on the value assigned to the variable.
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid);
+const feelings=["happy","sad","anxious"]
+//objects
+let myObj={
+    name:"aman",
+    age: 22,
+}
+//functions
+const myFunctions=function(){
+    console.log("hello world");
+}
+//non primitive datatypes are of function object type
