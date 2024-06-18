a4271841c6a2306275168b954304bff5090addb9
alwaysAman92 <134390829+alwaysAman92@users.noreply.github.com>
2024-06-18 17:12:00 +0000

    Math and num

---
+++ 01_basics/06_nums-and-maths.js
@@ -0,0 +1,22 @@
+/*const score=400
+console.log(score);
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toFixed(2));//can be used in e-commerce website to limit the decimal places of digits
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(3));
+
+const Hunderds=10000000
+console.log(Hunderds.toLocaleString('en-IN'));*/
+
+//******************Maths*******************
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.random());//gives value between 0 and 1
+console.log((Math.random()*10) + 1);//gives value between 1 and 9
+
+const min=10
+const max=20
+
+//(Math.floor(Math.random()*(max-min+1)) + min)
