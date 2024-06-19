e8d6de0f3c93c5c6dd3765caf7571b2e0e31d0e3
alwaysAman92 <134390829+alwaysAman92@users.noreply.github.com>
2024-06-19 10:35:24 +0000

    date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,11 @@
+//Dates
+//TC39 or technical committee 39 is working upon temporal objects which will be more efficient
+
+//let myDate=new Date();
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+//let myCreatedDate=new Date(2024,4,24) //(year,month,date)
+//console.log(myCreatedDate.toDateString());
+
+//console.log(Math.floor(Date.now()/1000));
